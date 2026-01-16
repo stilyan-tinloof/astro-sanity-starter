18a7edc809d25da686bf8b3834ce0584b8f02926
Seif Ghezala <seif@tinloof.com>
2026-01-16 19:51:20 +0400

    make studio work

--- packages/sanity/sanity.cli.ts
+++ packages/sanity/sanity.cli.ts
@@ -5,6 +5,9 @@ export default defineCliConfig({
     projectId: process.env["SANITY_STUDIO_PROJECT_ID"] || "rpxvvkoy",
     dataset: process.env["SANITY_STUDIO_DATASET"] || "migration",
   },
+  project: {
+    basePath: "/cms",
+  },
   typegen: {
     generates: "./sanity.types.ts",
     overloadClientMethods: true,
