03c5f91e9700707f0b832dd0de7c2aca8dea2198
stilyan-tinloof <stilyan-tinloof@users.noreply.github.com>
2026-01-25 16:51:12 +0000

    Add Tailwind CSS, fix CMS studio border

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,9 +5,15 @@ import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
   adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
